e2ebb9c902c9eea0f2eb1ffcb1c36f121bf95de2
michael@ubuntu-netbook <none@none>
2010-08-30 23:24:17 +0100

    started implementing provider

--- Mooduino/Db/Migrations/MigrationProvider.php
+++ Mooduino/Db/Migrations/MigrationProvider.php
@@ -5,13 +5,18 @@ require_once 'Mooduino/Db/Migrations/Migration.php';
 require_once 'Mooduino/Db/Migrations/Migration/Abstract.php';
 require_once 'Mooduino/Db/Migrations/MigrationManager.php';
 
-class Mooduino_Db_Migrations_MigrationProvider extends Zend_Tool_Framework_Provider_Abstract implements Mooduino_Db_Migrations_MigrationProvider_Interface {
+class Mooduino_Db_Migrations_MigrationProvider extends Zend_Tool_Project_Provider_Abstract implements Mooduino_Db_Migrations_MigrationProvider_Interface {
+
+	private $profile = null;
+	private $config = null;
+	private $dbAdapter = null;
 
 	public function getName() {
 		return 'Migration';
 	}
 
 	public function generate($name, $env='development') {
+		$this->init($env);
 		$this->_registry->getResponse()->appendContent(sprintf('Generating migration %s.', $name));
 	}
 
@@ -29,6 +34,36 @@ class Mooduino_Db_Migrations_MigrationProvider extends Zend_Tool_Framework_Provi
 
 	public function show($revision='all', $env='development') {
 
+	}
+	
+	private function init($env) {
+        $this->getConfig($env);
+	}
+	
+	private function getProfile() {
+		if (is_null($this->profile)) {
+			$this->profile = $this->_loadProfile(self::NO_PROFILE_THROW_EXCEPTION);
+		}
+		return $this->profile;
+	}
+	
+	private function getConfig($env) {
+		if (is_null($this->config)) {
+			$configFile = $this->getProfile()->search('applicationConfigFile');
+		    if ($configFile === false) {
+		        throw new Zend_Tool_Project_Exception('An application configuration file is required.');
+		    }
+		    $this->config = new Zend_Config_Ini($configFile->getPath(), $env);
+        }
+        return $this->config;
+	}
+	
+	private function getDbAdapter() {
+		if (is_null($this->dbAdapter)) {
+            $dbConfig = $this->getConfig()->resources->db;
+            $this->dbAdapter = Zend_Db::factory($dbConfig->adapter, $dbConfig->params);
+        }
+        return $this->dbAdapter;
 	}
 }
 
